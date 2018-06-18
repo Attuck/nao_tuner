3781a7b29d4b85b8dad447eeb52fa8f5242192f4
Ricardo Apú <rapuc@symbiotic.net>
2018-06-18 12:20:44 -0600

    added chords logic

--- main.py
+++ main.py
@@ -25,6 +25,14 @@ NAO_IP = "10.0.252.126" # Default
 positionErrorThresholdPos = 0.01
 positionErrorThresholdAng = 0.03
 
+NOTES_TO_FREQ ={:'La':864.00,
+                :'Mi':323.63,
+                :'Do':256.87,
+                :'Sol':384.87}
+PITCH_THRESHHOLD = 0.2
+SLEEP_TIME = 2
+def log_diff(n,m):
+    return np.log(n) - np.log(m)
 
 
 def main():
@@ -56,65 +64,84 @@ def main():
     pport)       # parent broker port
     try:    
         ##
+        global SoundReceiver
         global motionProxy
+        try:
+            SoundReceiver = SoundReceiverModule("SoundReceiver", pip, pport)
+            tts = naoqi.ALProxy("ALTextToSpeech", pip, pport)
+            naoqi.ALProxy('ALBasicAwareness',pip, pport).stopAwareness()
+            memProxy = naoqi.ALProxy("ALMemory",pip, pport)
+
+        except Exception,e:
+            print "Could not create proxy"
+            print "Error was: ",e
         motionProxy = naoqi.ALProxy("ALMotion", pip, pport)
 
-        # Warning: SoundReceiver must be a global variable
-        # The name given to the constructor must be the name of the
-        # variable
-
-
-        naoqi.ALProxy('ALBasicAwareness').stopAwareness()
-
+        """
+            Detecta 'Ayuda' y se mueve hacia el sonido
+        """
         MuevaLaCabeza()
         theHeadPosition = motionProxy.getPosition("Head", 0, False)
-        
         print theHeadPosition
-        
         x = 0.5
         y = 0.0
         Theta = theHeadPosition[4] 
         AnguloHeadYaw = theHeadPosition[4] 
         AnguloHeadPitch = theHeadPosition[5]   
-        RoteElCuerpo(Theta, AnguloHeadYaw, AnguloHeadPitch)    
-            
+        RoteElCuerpo(Theta, AnguloHeadYaw, AnguloHeadPitch)      
         MuevaseAUkelele(x, y, Theta)
-
-        global SoundReceiver
-        SoundReceiver = SoundReceiverModule("SoundReceiver", pip, pport)
         SoundReceiver.start()
-        time.sleep(5)
-        try:
-            # create proxy on ALMemory
-            memProxy = naoqi.ALProxy("ALMemory",pip, pport)
-            #getData
-            lastfreq = memProxy.getData("ultimaFreq")
-            print "ultimaFreq :", str(lastfreq)
-        except RuntimeError,e:
-            # catch exception
-            print "error read data", e
-            lastfreq = 0
+        """
+            Empieza a escuchar cada cuerda, calcula la frecuencia más cercana y dar retroalimentación para afinarla
+        """
+        time.sleep(SLEEP_TIME)
+        i = 0
+        afinando = True
+        notas = NOTES_TO_FREQ.keys()
+        while (afinando):
+            tts.say("Toque la cuerda número " str(i+1) ", que debe ser la nota " + notas[i])
+            try:
+                lastfreq =[memProxy.getData("freqs"+i) for i in range(0,10)]
+                print "ultimasFreq :", str(lastfreq)
+            except RuntimeError,e:
+                # catch exception
+                print "error read data", e
+                lastfreq = 0
 
-        try:
-            tts = naoqi.ALProxy("ALTextToSpeech", pip, pport)
             if lastfreq != 0:
-                tts.say("La frecuencia que escuché es  de" + str(lastfreq) + "hercios")
+                medianfreq = np.median(lastfreq)
+                tts.say("La frecuencia que escuché es  de" + str(medianfreq) + "hercios")
+                logdiff = log_diff(medianfreq,NOTES_TO_FREQ[notas[i]])
+                if abs(log_diff) <= PITCH_THRESHHOLD:
+                    tts.say("La cuerda está afinada Muy Bien!")
+                    if(i == len(NOTES_TO_FREQ)-1):
+                        afinando = False
+                    else:
+                        i+=1
+                else:
+                    if logdiff > 0:
+                        tts.say("La cuerda está muy alta, bájele")
+                    else:
+                        tts.say("La cuerda está muy baja, súbale")
             else:
-                tts.say("No pude escuchar ni picha, Adiós")
-        except Exception,e:
-            print "Could not create proxy to ALTextToSpeech"
-            print "Error was: ",e
-        SoundReceiver.STOP()
+                tts.say("No pude escuchar ni picha, Baaai")
+            time.sleep(SLEEP_TIME)
+            i+=1
+
+        """
+            Se devuele el robot al punto inicial y debería poder empezar de nuevo.
+        """
+        SoundReceiver.pause()
         DeMediaVuelta() 
         MuevaseAlCentro(x, y, Theta)
-        DeMediaVuelta() 
-
+        DeMediaVuelta()
         
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         print
         print "Interrupted by user, shutting down"
+        tts.say("Adios")
         myBroker.shutdown()
         sys.exit(0)
 
--- soundreciever.py
+++ soundreciever.py
@@ -22,39 +22,42 @@ class SoundReceiverModule(naoqi.ALModule):
 
     def __init__( self, strModuleName, strNaoIp, strNaoPort ):
         try:
-            naoqi.ALModule.__init__(self, strModuleName );
-            self.BIND_PYTHON( self.getName(),"callback" );
-            self.strNaoIp = strNaoIp;
-            self.strNaoPort = strNaoPort;
-            self.outfile = None;
+            naoqi.ALModule.__init__(self, strModuleName )
+            self.BIND_PYTHON( self.getName(),"callback" )
+            self.strNaoIp = strNaoIp
+            self.strNaoPort = strNaoPort
+            self.outfile = None
             self.aOutfile = [None]*(4-1); # ASSUME max nbr channels = 4
             # create proxy on ALMemory
             self.memProxy = naoqi.ALProxy("ALMemory",strNaoIp,strNaoPort)
+            self.pause = False
+            self.write_index = 0
+            self.MAX_INDEX = 10
 
         except BaseException, err:
             print( "ERR: abcdk.naoqitools.SoundReceiverModule: loading error: %s" % str(err) );
 
     # __init__ - end
     def __del__( self ):
-        print( "INF: abcdk.SoundReceiverModule.__del__: cleaning everything" );
-        self.stop();
+        print( "INF: abcdk.SoundReceiverModule.__del__: cleaning everything" )
+        self.stop()
 
     def start( self ):
-        audio = naoqi.ALProxy( "ALAudioDevice", self.strNaoIp, 9559 );
-        nNbrChannelFlag = 3; # ALL_Channels: 0,  AL::LEFTCHANNEL: 1, AL::RIGHTCHANNEL: 2; AL::FRONTCHANNEL: 3  or AL::REARCHANNEL: 4.
-        nDeinterleave = 0;
-        nSampleRate = SAMPLE_RATE;
-        audio.setClientPreferences( self.getName(),  nSampleRate, nNbrChannelFlag, nDeinterleave ); # setting same as default generate a bug !?!
-        audio.subscribe( self.getName() );
-        print( "INF: SoundReceiver: started!" );
+        audio = naoqi.ALProxy( "ALAudioDevice", self.strNaoIp, 9559 )
+        nNbrChannelFlag = 3 # ALL_Channels: 0,  AL::LEFTCHANNEL: 1, AL::RIGHTCHANNEL: 2; AL::FRONTCHANNEL: 3  or AL::REARCHANNEL: 4.
+        nDeinterleave = 0
+        nSampleRate = SAMPLE_RATE
+        audio.setClientPreferences( self.getName(),  nSampleRate, nNbrChannelFlag, nDeinterleave ) # setting same as default generate a bug !?!
+        audio.subscribe( self.getName() )
+        print( "INF: SoundReceiver: started!" )
 
     def stop( self ):
-        print( "INF: SoundReceiver: stopping..." );
-        audio = naoqi.ALProxy( "ALAudioDevice", self.strNaoIp, 9559 );
-        audio.unsubscribe( self.getName() );        
-        print( "INF: SoundReceiver: stopped!" );
+        print( "INF: SoundReceiver: stopping..." )
+        audio = naoqi.ALProxy( "ALAudioDevice", self.strNaoIp, 9559 )
+        audio.unsubscribe( self.getName() )
+        print( "INF: SoundReceiver: stopped!" )
         if( self.outfile != None ):
-            self.outfile.close();
+            self.outfile.close()
 
 
     def processRemote( self, nbOfChannels, nbrOfSamplesByChannel, aTimeStamp, buffer ):
@@ -67,52 +70,60 @@ class SoundReceiverModule(naoqi.ALModule):
         #~ for i in range( 8 ):
             #~ print( "%s (0x%x), " % (buffer[i],ord(buffer[i])) ),
         #~ print( "" );
-
-        aSoundDataInterlaced = np.fromstring( str(buffer), dtype=np.int16 );
-        #~ print( "len data: %s " % len( aSoundDataInterlaced ) );
-        #~ print( "data interlaced: " ),
-        #~ for i in range( 8 ):
-            #~ print( "%d, " % (aSoundDataInterlaced[i]) ),
-        #~ print( "" );
-        aSoundData = np.reshape( aSoundDataInterlaced, (nbOfChannels, nbrOfSamplesByChannel), 'F' );
-        #~ print( "len data: %s " % len( aSoundData ) );
-        #~ print( "len data 0: %s " % len( aSoundData[0] ) );
-            # compute fft
-        nBlockSize = nbrOfSamplesByChannel;
-        signal = aSoundData[0] * np.hanning( nBlockSize );
-        aFft = ( np.fft.rfft(signal) / nBlockSize );
-        fftData=abs(aFft)**2
-        # find the maximum
-        which = fftData[1:].argmax() + 1
-        # use quadratic interpolation around the max
-        if which != len(fftData)-1:
-            y0,y1,y2 = np.log(fftData[which-1:which+2:])
-            x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
-            # find the frequency and output it
-            thefreq = (which+x1)*SAMPLE_RATE/nBlockSize
-            if thefreq < 1100:
-                print "The freq is %f Hz." % (thefreq)
-                try:
-                    #insertData. Value can be int, float, list, string
-                    self.memProxy.insertData("ultimaFreq", thefreq)
-                except RuntimeError,e:
-                    # catch exception
-                    print "error insert data", e
-        else:
-            thefreq = which*SAMPLE_RATE/nBlockSize
-            if thefreq < 1100:
-                print "The freq is %f Hz." % (thefreq)
-                try:
-                    #insertData. Value can be int, float, list, string
-                    self.memProxy.insertData("ultimaFreq", thefreq)
-                except RuntimeError,e:
-                    # catch exception
-                    print "error insert data", e
-        return 0
+        if( self.pause ):
+            aSoundDataInterlaced = np.fromstring( str(buffer), dtype=np.int16 )
+            #~ print( "len data: %s " % len( aSoundDataInterlaced ) );
+            #~ print( "data interlaced: " ),
+            #~ for i in range( 8 ):
+                #~ print( "%d, " % (aSoundDataInterlaced[i]) ),
+            #~ print( "" );
+            aSoundData = np.reshape( aSoundDataInterlaced, (nbOfChannels, nbrOfSamplesByChannel), 'F' )
+            #~ print( "len data: %s " % len( aSoundData ) );
+            #~ print( "len data 0: %s " % len( aSoundData[0] ) );
+                # compute fft
+            nBlockSize = nbrOfSamplesByChannel
+            signal = aSoundData[0] * np.hanning( nBlockSize )
+            aFft = ( np.fft.rfft(signal) / nBlockSize )
+            fftData=abs(aFft)**2
+            # find the maximum
+            which = fftData[1:].argmax() + 1
+            # use quadratic interpolation around the max
+            if which != len(fftData)-1:
+                y0,y1,y2 = np.log(fftData[which-1:which+2:])
+                x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
+                # find the frequency and output it
+                thefreq = (which+x1)*SAMPLE_RATE/nBlockSize
+                if thefreq < 1100:
+                    print "The freq is %f Hz." % (thefreq)
+                    try:
+                        #insertData. Value can be int, float, list, string
+                        self.memProxy.insertData("freqs"+str(self.write_index % self.MAX_INDEX), thefreq)
+                        self.write_index+=1
+                    except RuntimeError,e:
+                        # catch exception
+                        print "error insert data", e
+            else:
+                thefreq = which*SAMPLE_RATE/nBlockSize
+                if thefreq < 1100:
+                    print "The freq is %f Hz." % (thefreq)
+                    try:
+                        #insertData. Value can be int, float, list, string
+                        self.memProxy.insertData("freqs"+ str(self.write_index % self.MAX_INDEX), thefreq)
+                        self.write_index+=1
+                    except RuntimeError,e:
+                        # catch exception
+                        print "error insert data", e
+            return 0
     # processRemote - end
 
+    def pause( self ):
+        self.pause = True
+        self.write_index = 0
+
+    def resume( self ):
+        self.pause = False
 
     def version( self ):
-        return "0.5";
+        return "0.5"
 
 # SoundReceiver - end
